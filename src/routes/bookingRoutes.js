e7b5c87f048cc29ec375c72c8b7dc2aadf3fec6f
d4rk_katt <shrest197@uni.coventry.ac.uk>
2025-07-12 15:54:23 +0545

    adding password requirement

--- src/routes/bookingRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookTicket } = require('../controllers/bookingController');
-
-router.post('/book', bookTicket);
-
-module.exports = router;
